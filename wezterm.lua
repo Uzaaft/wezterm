cab0c47b23a6f5d825c8df39cd737ba83f39547f
Uzair Aftab <uzaaft@outlook.com>
2023-11-06 07:30:50 +0100

    feat: use builder api for wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,93 +1,71 @@
+---@type Wezterm
 local wezterm = require "wezterm"
-
--- local launch_menu = {}
-local padding = {
-  left = "1cell",
-  right = "1cell",
-  top = "0.5cell",
-  bottom = "0.5cell",
-}
-
--- Reload the configuration every ten minutes
-wezterm.time.call_after(600, function()
-  wezterm.reload_configuration()
-end)
 
 -- A helper function for my fallback fonts
 local function font_with_fallback(name, params)
-  local names = { name, "mini-file-icons", "Hack Nerd Font", "SauceCodePro Nerd Font" }
-  return wezterm.font_with_fallback(names, params)
+ local names = { name, "mini-file-icons", "Hack Nerd Font", "SauceCodePro Nerd Font" }
+ return wezterm.font_with_fallback(names, params)
 end
 
-local function get_theme()
-  local _time = os.date("*t")
-  if _time.hour >= 1 and _time.hour < 9 then
-    return "Rosé Pine (base16)"
-  elseif _time.hour >= 9 and _time.hour < 17 then
-    return "tokyonight_night"
-  elseif _time.hour >= 17 and _time.hour < 21 then
-    return "Catppuccin Mocha"
-  elseif _time.hour >= 21 and _time.hour < 24 or _time.hour >= 0 and _time.hour < 1 then
-    return "kanagawabones"
-  end
-end
-
-return {
-  bidi_enabled = true,
-  bidi_direction = "AutoLeftToRight",
-  color_scheme = get_theme(),
-  font = font_with_fallback({
-    family = "FiraCode Nerd Font",
-    harfbuzz_features = {
-      "zero",
-    },
-  }),
-  font_rules = {
-    {
-      intensity = "Bold",
-      font = font_with_fallback {
-        family = "Liga SFMono Nerd Font",
-        harfbuzz_features = {
-          "zero",
-        },
-        weight = "Medium",
-      },
-    },
-    {
-      italic = true,
-      intensity = "Bold",
-      font = font_with_fallback {
-        family = "Iosevka Nerd Font",
-        -- family = "Dank Mono",
-        weight = "Medium",
-        italic = true,
-      },
-    },
-    {
-      italic = true,
-      font = font_with_fallback {
-        -- family = "Dank Mono",
-        family = "Iosevka Nerd Font",
-        weight = "Regular",
-        italic = true,
-      },
-    },
+local config = wezterm.config_builder()
+config.term = "wezterm"
+-- More speed
+config.front_end = "WebGpu"
+-- ALL the speed
+config.webgpu_power_preference = "HighPerformance"
+config.cursor_thickness = 2
+config.default_cursor_style = 'SteadyBar'
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.line_height = 1.2
+config.cell_width = 0.9
+config.tab_bar_at_bottom = true
+config.color_scheme = "Catppuccin Mocha"
+config.window_decorations = "RESIZE"
+config.audible_bell = "Disabled"
+config.hide_tab_bar_if_only_one_tab = true
+config.use_dead_keys = false
+config.selection_word_boundary = " \t\n{}[]()\"'`,;:@"
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+config.window_background_opacity = 0.70
+config.macos_window_background_blur = 100
+config.underline_position = -6
+config.underline_thickness = '210%'
+config.font = font_with_fallback {
+  family = "FiraCode Nerd Font",
+  harfbuzz_features = {
+   "zero",
   },
-  send_composed_key_when_left_alt_is_pressed = true,
-  send_composed_key_when_right_alt_is_pressed = true,
-  initial_cols = 128,
-  initial_rows = 32,
-  use_dead_keys = false,
-  window_padding = padding,
-  window_decorations = "RESIZE",
-  hide_tab_bar_if_only_one_tab = true,
-  selection_word_boundary = " \t\n{}[]()\"'`,;:@",
-  -- disable_default_key_bindings = true,
-  line_height = 1.25,
-  font_size = 12,
-  window_background_opacity = 0.95,
-  bold_brightens_ansi_colors = false,
-  -- swap_backspace_and_delete = false,
-  -- term = "wezterm",
-  -- freetype_load_target = "Light",
  }
+config.font_rules = {
+  {
+   intensity = "Bold",
+   font = font_with_fallback {
+    family = "Liga SFMono Nerd Font",
+    harfbuzz_features = {
+     "zero",
+    },
+    weight = "Medium",
+   },
+  },
+  {
+   italic = true,
+   intensity = "Bold",
+   font = font_with_fallback {
+    family = "Iosevka Nerd Font",
+    -- family = "Dank Mono",
+    weight = "Medium",
+    italic = true,
+   },
+  },
+  {
+   italic = true,
+   font = font_with_fallback {
+    -- family = "Dank Mono",
+    family = "Iosevka Nerd Font",
+    weight = "Regular",
+    italic = true,
+   },
+  },
+ }
+return config --[[@as Wezterm]]
